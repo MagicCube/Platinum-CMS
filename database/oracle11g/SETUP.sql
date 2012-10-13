d7c4217ca85f5c411d2cd73e3a4849836f900616
Henry.Li <Administrator@Henry-Macbook>
2012-10-13 21:21:42 +0800

    添加自动建库脚本。

--- database/oracle11g/SETUP.sql
+++ database/oracle11g/SETUP.sql
@@ -1,1 +1,8 @@
-﻿--accept system_pwd prompt '请输入SYSTEM的密码:' default 'system';
+﻿accept DB_NAME prompt '数据库实例名称:' default 'PTDB';
+accept DBS_PATH prompt '数据库表空间物理路径:' default '你的oradata/PTDB';
+start createTablespaces.sql &DBS_PATH;
+start createUsers.sql;
+connect PT_SYS/sys@&DB_NAME;
+start createTables.sql;
+start initData.sql;
+commit;
