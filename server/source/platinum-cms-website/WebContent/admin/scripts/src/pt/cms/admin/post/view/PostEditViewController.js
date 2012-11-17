0c064381b836fc14807db9b892e343d703701d58
Henry.Li <Administrator@Henry-Macbook>
2012-11-17 12:10:43 +0800

    添加 PostEditView。

---
+++ server/source/platinum-cms-website/WebContent/admin/scripts/src/pt/cms/admin/post/view/PostEditViewController.js
@@ -0,0 +1,40 @@
+$ns("pt.cms.admin.post.view");
+
+$include("$/pt/cms/admin/post/view/PostEditView.css");
+
+pt.cms.admin.post.view.PostEditViewController = function()
+{
+    var me = $extend(mx.view.ViewController);
+    var base = {};
+    
+    me.data = null;
+    
+    base.viewDidLoad = me.viewDidLoad;
+    me.viewDidLoad = function()
+    {
+        me.view.$element.addClass("PostEditView");
+        
+        base.viewDidLoad();
+        
+        me.toolbar = new pt.cms.admin.common.view.Toolbar();
+        me.toolbar.addButton("savePost", "保存").addClass("default");
+        me.toolbar.addButton("cancel", "取消").addClass("yellow").click(_btnCancel_onclick);
+        me.toolbars = [me.toolbar];
+        
+        me.renderView();
+    };
+    
+    me.renderView = function()
+    {
+        
+    };
+    
+    
+    
+    function _btnCancel_onclick(e)
+    {
+        adminPageController.popViewController();
+    }    
+    
+    return me.endOfClass(arguments);
+}
