f7a9e2fcc41643387866dcbf6577777d45215717
Henry.Li <Administrator@Henry-Macbook>
2012-11-17 12:10:08 +0800

    添加 getButton 接口。

--- server/source/platinum-cms-website/WebContent/admin/scripts/src/pt/cms/admin/common/view/Toolbar.js
+++ server/source/platinum-cms-website/WebContent/admin/scripts/src/pt/cms/admin/common/view/Toolbar.js
@@ -13,5 +13,25 @@ pt.cms.admin.common.view.Toolbar = function()
         me.$element.addClass("Toolbar");
     };
     
+    me.getButton = function(p_id)
+    {
+        return me.$element.children("button#" + p_id);
+    };
+    
+    me.addButton = function(p_id, p_text, p_onclick)
+    {
+        var $button = $("<button/>");
+        $button.attr("id", p_id);
+        $button.text(p_text);
+        $button.click(p_onclick);
+        return me.addElement($button);
+    };
+    
+    me.addElement = function(p_element)
+    {
+        me.$element.append(p_element);
+        return p_element;
+    };
+    
     return me.endOfClass(arguments);
 };
