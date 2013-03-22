d08d0787fcbf9e9207aca7c2f36888b8fd94f04d
Hutai <hutaizn@foxmail.com>
2013-03-22 17:44:11 +0800

    Hutai

--- server/source/platinum-cms-website/WebContent/admin/scripts/src/pt/cms/admin/post/view/PostEditViewController.js
+++ server/source/platinum-cms-website/WebContent/admin/scripts/src/pt/cms/admin/post/view/PostEditViewController.js
@@ -175,8 +175,8 @@ pt.cms.admin.post.view.PostEditViewController = function()
         $sideBar.append($dl);
         
         var $dl = $("<dl><dt>来源</dt> <dd></dd></dl>");
-        me.$category = $("<select id=category><option value=0>(空)</option></select>");
-        $dl.children("dd").append(me.$category);
+        me.$source = $("<input type=text id=source />");
+        $dl.children("dd").append(me.$source);
         $sideBar.append($dl);
         
         var $dl = $("<dl><dt>发布者</dt> <dd></dd></dl>");
