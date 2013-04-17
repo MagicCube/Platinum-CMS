b7ad4c18561f362734111d68c4416615358a248f
Hutai <Hutai@h280106778>
2013-04-17 20:15:33 +0800

    hutai

--- server/source/platinum-cms-website/WebContent/static/common/scripts/lib/bxslider/select.js
+++ server/source/platinum-cms-website/WebContent/static/common/scripts/lib/bxslider/select.js
@@ -19,11 +19,11 @@ function select(flag){
 	for(i = 1;i<=4;i++){
 		if(flag==i){
 			$("#post"+i).css({"display":"block"});
-			$("#sel"+i).css({"background-color":"#78ac5a","color":"#CCC"});
+			$("#sel"+i).css({"background-color":"#468d00","color":"#CCC"});
 		}
 		else{
 			$("#post"+i).css({"display":"none"});
-			$("#sel"+i).css({"background-color":"#CCC","color":"#78ac5a"});
+			$("#sel"+i).css({"background-color":"#CCC","color":"#468d00"});
 		}
 	}
 }
